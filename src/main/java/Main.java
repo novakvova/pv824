a2616c7b0bc9019c6bccce64388a473027c767aa
Volodymyr Novak <novakvova@gmail.com>
2020-08-22 13:30:22 +0300

    Delete role and menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,35 +3,81 @@ import org.hibernate.Session;
 import entities.Role;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.postgresql.core.SqlCommand;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args) {
-        System.out.println("Добавлення ролі адмін");
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("Добавлення ролі адмін");
+        int action=0;
+        Session session = HibernateSessionFactoryUtil
+                .getSessionFactory().openSession();
         try {
-            Session session = HibernateSessionFactoryUtil
-                    .getSessionFactory().openSession();
-            Transaction tx1 = session.beginTransaction();
-           // Role role = new Role("Admin");
-           // session.save(role);
-          //  tx1.commit();
-           // session.close();
+            do {
+                System.out.println("0. Вихід.");
+                System.out.println("1. Додати роль.");
+                System.out.println("2. Список ролей.");
+                System.out.println("3. Видалення ролей.");
+                action = Integer.parseInt(scanner.nextLine());
+                switch (action) {
+                    case 1: {
+                        try {
+                            Transaction tx1 = session.beginTransaction();
+                            String userRole;
+                            System.out.println("Введіть роль:");
+                            userRole = scanner.nextLine();
+                            Role role = new Role(userRole);
+                            session.save(role);
+                            tx1.commit();
+                            System.out.println("-----Додвання успішне-----");
+                        } catch (Exception ex) {
+                            System.out.println("Довання не успішне ОТВАЛ!!!");
+                        }
+                        break;
+                    }
 
-            Scanner scanner = new Scanner(System.in);
-            String userRole;
-            System.out.println("Введіть роль:");
-            userRole = scanner.nextLine();
-            Role role = new Role(userRole);
-            session.save(role);
-            tx1.commit();
-            session.close();
+                    case 2: {
+                        try {
+                            System.out.println("Список ролей");
+                            String hql = "FROM Role";
+                            Query query = session.createQuery(hql);
+                            List<Role> results = query.list();
+                            for (Role role : results) {
+                                System.out.println("Id = "+role.getId()+"\t"+role.getName());
+                            }
+                        } catch (Exception ex) {
+                            System.out.println("Читання даних ОТВАЛ!!!");
+                        }
+                        break;
+                    }
 
+                    case 3: {
+                        try {
+                            Transaction tx1 = session.beginTransaction();
+                            System.out.println("Введіть іd ролі:");
+                            int id = Integer.parseInt(scanner.nextLine());
+                            Role role =  HibernateSessionFactoryUtil.getSessionFactory()
+                                    .openSession().get(Role.class, id);
+                            session.delete(role);
+                            tx1.commit();
+                            System.out.println("Успішно видалено");
+                        } catch (Exception ex) {
+                            System.out.println("Видалення даних ОТВАЛ!!!"+ ex.getMessage());
+                        }
+                        break;
+                    }
+                }
+            }
+            while(action != 0);
         }
         catch (Exception ex) {
             System.out.println("Проблема при додавані ролі "+ex.getMessage());
         }
-        System.out.println("Якщо ви ще бачите. значить усе норм.");
+        session.close();
 
 
     }
